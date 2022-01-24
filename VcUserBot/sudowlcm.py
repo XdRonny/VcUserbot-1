1a63322797220c4d1ed782c11b9e0998d2232e9d
akshit <akshit2022@outlook.com>
2022-01-24 14:13:00 +0530

    via file delete

--- VcUserBot/sudowlcm.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, emoji, filters
-from config import HNDLR, SUDO_USERS
-from pyrogram.types import Message
-
-@Client.on_message(filters.new_chat_members & filters.user(SUDO_USERS))
-async def sudowlcm(client, m: Message):
-    await m.reply_text("**Hey Here Joined My SUDO User**")
