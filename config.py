2720793ad3c9553414d98c67cbfb41fb02206df9
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-01-24 13:20:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ if os.path.exists(".env"):
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
-HNDLR = list(getenv("HNDLR", "/ ! .").split())
+HNDLR = list(os.getenv("HNDLR", "/ ! .").split())
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", None)
-OFFICIAL_UPSTREAM_REPO = os.getenv("OFFICIAL_UPSTREAM_REPO", "https://github.com/darkphoenix2601/VcUserbot")
+OFFICIAL_UPSTREAM_REPO = os.getenv("OFFICIAL_UPSTREAM_REPO", "https://github.com/vaibhavchandra13/VcUserBot")
 
 contact_filter = filters.create(
     lambda _, __, message: (message.from_user and message.from_user.is_contact)
